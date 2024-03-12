e3aa9233ddee7e721842f505532c61aca9ba94a7
shabbir-anjum <mahrshabbir768@gmail.com>
2024-03-12 13:09:47 +0500

    main

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { faLocationDot, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCartShopping, faFlag, faLocationDot, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
@@ -11,47 +11,70 @@ export default function Header() {
                         <img src="../images/amazon_logo.png" alt="error" className=' w-24 h-10' />
                     </div>
                     <div className=' flex items-center gap-1  hover:border p-1 border-transparent'>
-                       
-                            
+
+
                         <div className=' text-gray-400 text-sm'>
                             <span className='pl-4'> Deliver to</span>
-                           
-                            <div>
-                            <FontAwesomeIcon icon={faLocationDot} className='mr-2 h-3' />
-                            <span className=' font-Roboto text-white'>Pakistan</span>
+
+                            <div className='flex'>
+                                <FontAwesomeIcon icon={faLocationDot} className='mr-2 h-3  text-3xl' />
+                                <span className=' font-Roboto text-white text-base'>Pakistan</span>
                             </div>
-                            
+
                         </div>
                     </div>
 
-                    <div className="mb-3 relative overflow-hidden rounded-md text-black">
-    <div className='absolute left-0 bg-moonColor border-r-1 border-black h-full'>
-        <select name="option" id="option" className="h-full outline-none bg-transparent border-r border-gray-300">
-            <option value="All">All</option>
-            <option value="Books">Books</option>
-            <option value="Electronics">Electronics</option>
-        </select>
-    </div>
-    <input type="email" className="form-control pl-10" id="exampleFormControlInput1" placeholder="name@example.com" />
-    <div className='absolute right-0 top-0 bg-peechColor h-full'>
-       <FontAwesomeIcon icon={faSearch} className='p-2'/>
-    </div>
-</div>
+                    <div className=" text-black flex h-12 border-transparent hover:border-3  rounded-xl">
+                        <select name="name" id="id" className=' rounded-lg rounded-r-none bg-moonColor'>
+                            <option value="all">All</option>
+                            All</select>
+                        <input type="text" className=' w-[780px] pl-4 text-base outline-none'  placeholder='Search Amazon'/>
+                        <div className=' bg-peechColor p-2 text-xl rounded-lg rounded-l-none'>
+                            <FontAwesomeIcon icon={faSearch} className=' text-3xl'/>
+                        </div>
+                    </div>
 
 
 
-                    <div>
-                        country
+                    <div className='flex  items-center hover:border p-2 border-transparent' >
+                        <FontAwesomeIcon icon={faFlag}/>
+                        <select name="nume" id="d" className=' bg-transparent outline-none'>
+                            <option value="vl">EN</option>
+                            EN</select>
                     </div>
-                    <div>
-                        account list
+                    <div className=' hover:border p-2 border-transparent' >
+                    <div className=' text-xs pl-1'>hello,SignIn</div>
+                        <select name="nume" id="d" className=' bg-transparent outline-none'>
+                            <option value="vl">
+                            <div className=' text-base font-bold'>
+                                Account & List
+                            </div></option>
+                            
+                            </select>
                     </div>
-                    <div>
-                        ordder
+                    <div className=' hover:border p-2 border-transparent' >
+                        <div className='text-xs'>Returns</div>
+                        <div className=' font-Roboto font-bold'>&Orders</div>
                     </div>
-                    <div>
-                        icon
+                    <div className=' hover:border p-2 border-transparent' >
+                        <FontAwesomeIcon icon={faCartShopping} className=' text-3xl'/>
+                        <span className='font-Roboto font-bold'> Cart</span>
                     </div>
+                </div>
+                <div className='flex items-center h-12 p-3 bg-navColor text-white '>
+                    <div className='flex gap-2 items-center hover:border p-2 border-transparent'>
+                        <FontAwesomeIcon icon={faBars} className=' text-xl'/>
+                        <div>All</div>
+                    </div>
+                    
+                    <ul className='flex gap-8 mt-3 items-center'>
+                        <li className=' hover:border p-2 border-transparent' >Today's Deal</li>
+                        <li className=' hover:border p-2 border-transparent' >Customer Service</li>
+                        <li className=' hover:border p-2 border-transparent' >Resgistry</li>
+                        <li className=' hover:border p-2 border-transparent' >Gift Cards</li>
+                        <li className=' hover:border p-2 border-transparent' >Cells</li>
+                    </ul>
+                    
                 </div>
             </header>
 
