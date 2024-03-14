d59f230fb5bb8c39837461a13b42df9b236dff62
shabbir-anjum <mahrshabbir768@gmail.com>
2024-03-14 17:31:46 +0500

    main

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <>
+        <section className=' h-80 overflow-hidden'>
+            <img src="/public/images/amazon_logo.png" alt="" />
+        </section>
+    </>
+  )
+}
