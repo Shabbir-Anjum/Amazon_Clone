7d133691f7b26f173a5fa11a61d17ab81c0bd661
shabbir-anjum <mahrshabbir768@gmail.com>
2024-03-14 18:07:04 +0500

    main

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from 'swiper/modules';
 export default function Hero() {
   return (
     <>
-        <section className=' h-80 overflow-hidden'>
-            <img src="/public/images/amazon_logo.png" alt="" />
+        <section className=' h-screen'>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper h-[70%]">
+        <SwiperSlide className="bg-[url('../images/Swiper/1.jpg')]"></SwiperSlide>
+        <SwiperSlide className="bg-[url('../images/Swiper/2.jpg')]"></SwiperSlide>
+        <SwiperSlide className="bg-[url('../images/Swiper/3.jpg')]"></SwiperSlide>
+        <SwiperSlide className="bg-[url('../images/Swiper/4.jpg')]"></SwiperSlide>
+        <SwiperSlide className="bg-[url('../images/Swiper/5.jpg')]"></SwiperSlide>
+        <SwiperSlide className="bg-[url('../images/Swiper/6.jpg')]"></SwiperSlide>
+      </Swiper>
         </section>
     </>
   )
